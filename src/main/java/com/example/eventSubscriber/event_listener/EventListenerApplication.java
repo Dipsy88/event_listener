73fd20f6a0dd9d413c40d9c7f213d7fb4199b733
dipsy88 <dipsy.urfriend@gmail.com>
2018-11-08 10:52:44 +0100

    first commit

---
+++ src/main/java/com/example/eventSubscriber/event_listener/EventListenerApplication.java
@@ -0,0 +1,12 @@
+package com.example.eventSubscriber.event_listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventListenerApplication.class, args);
+	}
+}
